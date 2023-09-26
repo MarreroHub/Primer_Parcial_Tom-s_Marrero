2e91cc7f37564099cb81fd5df2fb4ce514db9aae
MarreroTom <tomarrero96@gmail.com>
2023-09-26 14:24:43 -0300

    Adding terrain (again), Skybox and Vicotry Scene Advance Condition 3/3

--- 1er_Parcial_Marrero/Assets/Scripts/TargetScript.cs
+++ 1er_Parcial_Marrero/Assets/Scripts/TargetScript.cs
@@ -2,13 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TargetScript : MonoBehaviour
 {
     public GameManagment gameManager;
     private void OnTriggerEnter()
     {
-        gameManager.CompleteLevel();
+        SceneManager.LoadScene(3);
+       //ameManager.CompleteLevel();
     }
 
 }
